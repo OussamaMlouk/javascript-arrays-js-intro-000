d713158cb8c80f1f242347677b0561eac83832ed
Oussama Mlouk <o.mlouk@hotmail.co.uk>
2018-10-10 20:44:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = [`snickers`, `hundred grand`, `kitkat`, `skittles`]
 
 function addElementToBeginningOfArray(array,element){
-return [foo, ...array]
+return [element, ...array]
 }
 
 //var array = [1]
