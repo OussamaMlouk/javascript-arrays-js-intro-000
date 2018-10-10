676cf49ad80152e8b270e23708ecd6e05a65ef1e
Oussama Mlouk <o.mlouk@hotmail.co.uk>
2018-10-10 20:39:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,6 +6,6 @@ function addElementToBeginningOfArray([1],"foo"){
 
 var array = [1]
 
-function destructivelyAddElementToBeginningOfArray([1], "foo"){
-  return [1].unshift("foo")
+function destructivelyAddElementToBeginningOfArray(array, "foo"){
+  return array.unshift("foo")
 }
